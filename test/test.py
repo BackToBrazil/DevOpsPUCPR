edbb35014664b9c19dbe0754589003a59f730e4e
BackToBrazil <kelvin.voltess@gmail.com>
2025-04-25 15:11:54 -0300

    asd

--- test/test.py
+++ test/test.py
@@ -5,19 +5,17 @@ from src.main import divide
 from src.main import power
 
 class TestCalculator:
-    def test_add(self, a, b):
-        assert add() ==  a + b
+    def test_add(self):
+        assert add(3, 4) ==  7
 
-    def test_subtract(self, a, b):
-        assert subtract() == a - b
+    def test_subtract(self):
+        assert subtract(10, 5) == 5
 
-    def test_multiply(self, a, b):
-        assert multiply() == a * b
+    def test_multiply(self):
+        assert multiply(2, 3) == 6
 
-    def test_divide(self, a, b):
-        if b == 0:
-            raise ValueError("Cannot divide by zero.")
-        assert divide() == a / b
+    def test_divide(self):
+        assert divide(10, 2) == 5
 
-    def test_power(self, a, b):
-        assert power() == a ** b
+    def test_power(self):
+        assert power(2, 3) == 8
