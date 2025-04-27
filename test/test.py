af85ec0e9316fb43aa8a7eafbb703bf4132a78bc
BackToBrazil <kelvin.voltess@gmail.com>
2025-04-27 10:19:14 -0300

    aaa

--- test/test.py
+++ test/test.py
@@ -1,18 +1,18 @@
 from src.main import *
 
 class TestCalculator:
-    def test_add(self):
+    def test_add():
         assert add(3, 4) ==  7
 
-    def test_subtract(self):
+    def test_subtract():
         assert subtract(10, 5) == 5
 
-    def test_multiply(self):
+    def test_multiply():
         assert multiply(2, 3) == 6
 
-    def test_divide(self):
+    def test_divide():
         assert divide(10, 2) == 5
 
-    def test_power(self):
+    def test_power():
         assert power(2, 3) == 8
 
